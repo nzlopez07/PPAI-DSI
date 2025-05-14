9415e0b8487ca13ae5942e29fcf2897bb1f1fe13
Nico <nzlopez07@gmail.com>
2025-05-14 16:29:03 -0300

    inicio de logica

--- controllers/GestorRevisionEventoSismico.py
+++ controllers/GestorRevisionEventoSismico.py
@@ -1,18 +1,33 @@
 from datetime import datetime
 from entities.EventoSismico import EventoSismico
+
 class GestorRevisionEventoSismico:
-    def __init__(self, eventosSismicos):
-        self.__eventosSismicos = eventosSismicos
+    def __init__(self, eventos):
+        self.horaFechaFinCambioEstado = None
+        self.eventosSismicos: list[EventoSismico] = eventos # Colección de todos los eventos sísmicos
+        self.eventosSismicosAutoDetectados: list[EventoSismico] = [] # Colección de todos los eventos sísmicos con estado actual "AutoDetectado"
 
     #Métodos get y set si corresponden
 
-    def calcularHoraFechaActual(self):
+    # Lógica del CU
+    def calcularFechaHoraFinCambioEstado(self):
     # Método para calcular fecha y hora en el momento    
-        return datetime.now()
+        self.horaFechaFinCambioEstado = datetime.now()
     
     def buscarEventosAutoDetectados(self):
-        eventosAutoDetectados = []
-        for evento in self.__eventosSismicos:
-            if evento.EstaAutoDetectado():
-                eventosAutoDetectados.append(evento)
-        return eventosAutoDetectados
+        # Recorre la colección de todos los eventos sísmicos y valida que tengan el estado "AutoDetectado"
+        for evento in self.eventosSismicos:
+            if evento.estaAutoDetectado():
+                self.eventosSismicosAutoDetectados.append(evento)
+
+    def ordenarPorFechaYHora():
+        pass
+
+    def obtenerEstacionesSismográficas():
+        pass
+
+    def clasificarMuestrasPorEstaciones():
+        pass
+
+    def llamarCU18():
+        pass
