fff1c0ca33ec66199106fd3e3e100ffc3f24af8c
Santiago <santiago2608rojas@gmail.com>
2025-05-14 13:28:11 -0300

    Definición clase AlcanceSismo

---
+++ entities/AlcanceSismo.py
@@ -0,0 +1,18 @@
+class AlcanceSismo:
+    def __init__(self, descripcion, nombre):
+        self.descripcion = descripcion
+        self.nombre = nombre
+
+    # Métodos GET
+    def getDescripcion(self):
+        return self.descripcion
+
+    def getNombre(self):
+        return self.nombre
+
+    # Métodos SET
+    def setDescripcion(self, descripcion):
+        self.descripcion = descripcion
+
+    def setNombre(self, nombre):
+        self.nombre = nombre
